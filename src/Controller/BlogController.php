9a02dff18c8ac217bba91e2c9c0204b94617b567
chikungugna <thomas.rey@posteo.net>
2019-05-11 13:45:03 +0200

    exercice routes avancées ok

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,6 +10,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+	/**
+	 * @Route("/blog/show/{slug}",
+	 *     requirements = {"slug" = "[a-z0-9\-]+"},
+	 *     defaults = {"slug" = "article-sans-titre"},
+		 *	  name = "blog_show"
+ *     		)
+	 *
+	 */
+	public function show($slug){
+
+		$slug = ucwords(str_replace('-', ' ', $slug));
+
+		return $this->render('blog/show.html.twig', ['slug'=>$slug]);
+
+	}
+
+
+
 	/**
 	 * @Route("/Blog/index")
 	 */
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,37 @@ namespace App\Controller;
 Use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route ("/Home", name="app_")
+ */
 class DefaultController extends AbstractController
 {
 
-	/**
-	 * @Route ("/", name="app_index")
-	 */
-	public function index(){
 
-		return $this->render('default.html.twig');
+
+	/**
+	 * @Route("/blog/{page}",
+	 *     	 methods = {"GET"},
+	 *      defaults = {"page" = 1 },
+	 *     requirements = {"page" = "\d+"},
+	 *     name = "index")
+	 */
+	public function index($page){
+
+		return $this->render('default.html.twig', ['page'=>$page]);
 
 	}
+
+	/**
+	 * @route("/blog/redirecting",
+	 *     		name = "redirecting")
+	 */
+	public function redirecting(){
+
+		return $this->redirectToRoute("app_index", ['page' => 23]);
+
+	}
+
+
 }
